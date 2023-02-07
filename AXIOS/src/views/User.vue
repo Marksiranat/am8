da143b6d7d2a2e3532f4c89b09a987365ff99bf0
Marksiranat <siranat2546@gmail.com>
2023-02-07 20:17:43 +0700

    am8

--- AXIOS/src/views/User.vue
+++ AXIOS/src/views/User.vue
@@ -29,15 +29,9 @@
       <br />
     </div>
 
-    <div class="d-flex justify-content-center">
-      <button @click="loadUser()">แสดงรายชื่อหนัง <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
-  <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
-</svg></button>
-    </div>
-
     <!---<div>{{ userData }}</div>-->
     <div class="container">
-      <div class="row" >
+      <div class="row">
         <div class="col-3 m-5" v-for="user in userData" :key="user.id">
           <div class="card mt-3" style="width: 18rem">
             <img :src="user.show.image.original" class="card-img-top" alt="..." />
@@ -48,9 +42,20 @@
                 *TYPE* : {{ user.show.type }}<br />
                 *LANGUADE* : {{ user.show.language }}<br />
 
-                <a :href="user.show.url"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-browser-chrome" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M16 8a8.001 8.001 0 0 1-7.022 7.94l1.902-7.098a2.995 2.995 0 0 0 .05-1.492A2.977 2.977 0 0 0 10.237 6h5.511A8 8 0 0 1 16 8ZM0 8a8 8 0 0 0 7.927 8l1.426-5.321a2.978 2.978 0 0 1-.723.255 2.979 2.979 0 0 1-1.743-.147 2.986 2.986 0 0 1-1.043-.7L.633 4.876A7.975 7.975 0 0 0 0 8Zm5.004-.167L1.108 3.936A8.003 8.003 0 0 1 15.418 5H8.066a2.979 2.979 0 0 0-1.252.243 2.987 2.987 0 0 0-1.81 2.59ZM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
-</svg>Browser</a
+                <a :href="user.show.url"
+                  ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-browser-chrome"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16 8a8.001 8.001 0 0 1-7.022 7.94l1.902-7.098a2.995 2.995 0 0 0 .05-1.492A2.977 2.977 0 0 0 10.237 6h5.511A8 8 0 0 1 16 8ZM0 8a8 8 0 0 0 7.927 8l1.426-5.321a2.978 2.978 0 0 1-.723.255 2.979 2.979 0 0 1-1.743-.147 2.986 2.986 0 0 1-1.043-.7L.633 4.876A7.975 7.975 0 0 0 0 8Zm5.004-.167L1.108 3.936A8.003 8.003 0 0 1 15.418 5H8.066a2.979 2.979 0 0 0-1.252.243 2.987 2.987 0 0 0-1.81 2.59ZM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+                    /></svg
+                  >Browser</a
                 ><br />
               </p>
             </div>
@@ -58,13 +63,12 @@
         </div>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const userData = ref([]);
 const url = ref("http://api.tvmaze.com/search/shows?q=golden%20girls");
@@ -80,6 +84,9 @@ function loadUser() {
       console.log(err);
     });
 }
+onMounted(() => {
+  loadUser();
+});
 </script>
 
 <style></style>
